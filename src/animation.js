f22b16d5a9421d6f323ed83845d908f5b7eafc5f
Yahya Zakaria <yz3004629@gmail.com>
2024-07-23 03:06:32 +0300

    update

--- src/animation.js
+++ src/animation.js
@@ -2,6 +2,13 @@ const toggleInfo = document.querySelector("#info-btn");
 const info = document.querySelector(".information-content");
 const infoLayer = document.querySelector(".info-layer");    
 const mainHeight = $(info).height();
+
+$(function () {
+    $(".loadingPage").css("opacity", "0");
+    $(".loadingPage").css("z-index", "-1111");
+    
+    
+})
 toggleInfo.addEventListener("click", () => {
   if ($(info).height() <= 650) {
     info.style.cssText = "height: 100% !important; overflow: visible; ";

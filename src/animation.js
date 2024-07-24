f6fc7be54079fb7980c6590de804450440757ee2
Yahya Zakaria <yz3004629@gmail.com>
2024-07-25 02:42:39 +0300

    update

--- src/animation.js
+++ src/animation.js
@@ -1,25 +1,30 @@
 const toggleInfo = document.querySelector("#info-btn");
 const info = document.querySelector(".information-content");
-const infoLayer = document.querySelector(".info-layer");    
+const infoLayer = document.querySelector(".info-layer");
 const mainHeight = $(info).height();
 
 $(function () {
-    setTimeout(() => {
-        
-        $(".loadingPage").css("opacity", "0");
-        $(".loadingPage").css("z-index", "-1111");
-    },1000)
-    
-    
-})
+  setTimeout(() => {
+    $(".loadingPage").css("opacity", "0");
+    $(".loadingPage").css("z-index", "-1111");
+  }, 1000);
+});
 toggleInfo.addEventListener("click", () => {
   if ($(info).height() <= 650) {
     info.style.cssText = "height: 100% !important; overflow: visible; ";
     infoLayer.style.cssText = "opacity: 0;";
-    $(toggleInfo).find("i").removeClass("fa-arrow-down").addClass("fa-arrow-up");
-} else {
+    $(toggleInfo)
+      .find("i")
+      .removeClass("fa-arrow-down")
+      .addClass("fa-arrow-up");
+  } else {
     info.style.cssText = `height: ${mainHeight}px !important; overflow: hidden; `;
     infoLayer.style.cssText = "opacity: 1;";
-    $(toggleInfo).find("i").removeClass("fa-arrow-up").addClass("fa-arrow-down");
+    $(toggleInfo)
+      .find("i")
+      .removeClass("fa-arrow-up")
+      .addClass("fa-arrow-down");
   }
 });
+
+

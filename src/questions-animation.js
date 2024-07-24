4dfc26bf560042869bce61cfb4448032763fd87c
Yahya Zakaria <yz3004629@gmail.com>
2024-07-25 02:54:58 +0300

    update

--- src/questions-animation.js
+++ src/questions-animation.js
@@ -3,10 +3,6 @@ const questionContent = document.querySelectorAll(".question-content");
 
 questionsBtns.forEach((btn) => {
   btn.addEventListener("click", (e) => {
-    questionContent.forEach((content) => {
-      content.classList.remove("d-block");
-      btn.children[0].children[1].children[0].classList.add("fa-chevron-down");
-    });
     btn.children[0].children[1].children[0].classList.contains(
       "fa-chevron-down"
     )
@@ -19,6 +15,5 @@ questionsBtns.forEach((btn) => {
           "fa-chevron-down"
         );
     btn.children[1].classList.toggle("d-block");
-    btn.children[1].classList.toggle("mt-5");
   });
 });

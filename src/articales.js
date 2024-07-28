41aa816aa0a605ab252b1e4b030f0d4fdeaac04c
Yahya Zakaria <yz3004629@gmail.com>
2024-07-28 19:23:10 +0300

    update

--- src/articales.js
+++ src/articales.js
@@ -1,16 +1,18 @@
 const articals = document.querySelectorAll(".articale");
 const articalBtns = document.querySelectorAll(".articaleInnerBtn button");
 const articaleContainer = document.querySelectorAll(".articaleBtn");
+const homeSection = document.querySelector("#home");
 articalBtns.forEach((btn) => {
     btn.addEventListener("click", (e) => {
-        articalBtns.forEach((articalBtn) => {
-          articalBtn.classList.remove("articaleBtnActive");
-        })
+      window.scrollTo(0, homeSection.offsetTop + homeSection.clientHeight );
+      articalBtns.forEach((articalBtn) => {
+        articalBtn.classList.remove("articaleBtnActive");
+      })
       let BtnTarget =  e.target.getAttribute("to");
       e.target.classList.add("articaleBtnActive");
-       articals.forEach((artical) => {
-           if (artical.getAttribute("id") == BtnTarget){
-             artical.classList.remove("d-none");
+      articals.forEach((artical) => {
+        if (artical.getAttribute("id") == BtnTarget){
+          artical.classList.remove("d-none");
              artical.classList.add("d-block");
            }else{
              artical.classList.remove("d-block");
